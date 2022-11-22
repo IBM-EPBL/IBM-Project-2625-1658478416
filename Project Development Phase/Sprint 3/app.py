fb350385cf869a2ff1c24ea6ae68fe1b067dc329
Nivedha <63503418+Nivedha22@users.noreply.github.com>
2022-11-22 15:50:52 +0530

    Add files via upload

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -4,6 +4,16 @@ import numpy as np
 from flask import render_template, request
 from flask_cors import CORS
 
+import requests
+import json
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "zJXNS7RuxksoVPyMjZDER8bqMSbF3OvzzuZZWw5FnpMU"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
 app = flask.Flask(__name__,template_folder='template')
 CORS(app)
  
@@ -62,15 +72,29 @@ def prediction():
 	Loan_amount = int(Loan_amount)
 	Loan_amount_term = int(Loan_amount_term)
 	credit = int(credit)
-	x =np.array([[0,gender, married, dependat,education,employ,annual_income,co_income,Loan_amount,Loan_amount_term,credit,proper]])
-	model = joblib.load('R.pkl')
-	ans = int(model.predict(x)[0])
-	if (ans==1):
-		print("You are eligible. Kindly wait for further notice")
-	else:
-		print("Your application status did not match our criteria")
-	return render_template('output.html', output=ans)
+	#x =np.array([[0,gender, married, dependat,education,employ,annual_income,co_income,Loan_amount,Loan_amount_term,credit,proper]])
+	x =[0,gender, married, dependat,education,employ,annual_income,co_income,Loan_amount,Loan_amount_term,credit,proper]
+	#model = joblib.load('R.pkl')
 	
+	#ans = int(model.predict(x)[0])
+	#if (ans==1):
+		#print("You are eligible. Kindly wait for further notice")
+	#else:
+		#print("Your application status did not match our criteria")
+
+	payload_scoring = {"input_data": [{"field": ["","Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area"], "values": [[0,0,0,0,0,0,4547,0.0,115.0,360,1,1], [0,0,0,0,0,0,4947,0.0,105.0,361,1,1]]}]}
+
+	response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/77f857ff-c248-41f7-b24f-2a3128ac1906/predictions?version=2022-11-22', json=payload_scoring,
+	headers={'Authorization': 'Bearer ' + mltoken})
+	print("Scoring response")
+	pred = response_scoring.json()
+	p = pred['predictions'][0]['values'][0][0]
+	if(pred == 0):
+		print("You are not eligible")
+	else:
+		print("You are eligible")
+	
+	return render_template('output.html', output=p)
 
 if __name__ == '__main__':
 	app.debug = True
